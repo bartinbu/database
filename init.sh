b117a833fdd30997612913943e727d612e063ceb
bartinbu <raspberrypi.bartin@gmail.com>
2022-11-25 12:41:03 -0500

    AutoCommit Fri Nov 25 12:41:03 PM EST 2022

--- init.sh
+++ init.sh
@@ -4,9 +4,8 @@ echo "------------------------Go to github settings and click 'add SSH key' with
 cat ~/.ssh/id_rsa.pub
 echo "-----------------------------------------Press enter after copy.--------------------------------------------"
 read pulse
-cd Desktop
-git clone "git@github.com:bartinbu/database.git"
-cd database
+cd Desktop/database
+
 git config --global init.defaultBranch origin main
 git config --global user.email "raspberrypi.bartin@gmail.com"
 git config --global user.name "bartinbu"
