f5d86571a569d9d054df5c848337b2ee7f25f474
bartinbu <119061329+bartinbu@users.noreply.github.com>
2023-02-25 14:23:53 +0300

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,6 +1,19 @@
+#!/bin/bash
 cd ~
-echo "Node Name:"
+echo "Node Name"
 read nodeName
+echo "Node Type"
+echo "1 - Humidity Sensor"
+echo "2 - Distance sensor"
+read typeID
+
+if (( $typeID == 2 )) ; then
+    nodeName+="_Distance"   
+elif (( $typeID == 1 )) ; then
+    nodeName+="_Humidity"
+else
+    echo "Invalid choice plase start again!"
+fi
 ssh-keygen -t rsa
 echo "------------------------Go to github settings and click 'add SSH key' with this key.------------------------"
 cat ~/.ssh/id_rsa.pub
@@ -16,9 +29,10 @@ git branch $nodeName
 git checkout $nodeName
 git push --set-upstream origin $nodeName
 pip install -r requirements.txt
-cd ..
+
 echo "[NODEINFO]
-nodename = $nodeName
-sensorsize = 5
-">config.ini
-(sudo crontab -l; echo "* * * * * /home/pi/database/sync.sh") | sort -u | sudo crontab -
+    nodename = $nodeName
+    nodetype = $typeID
+    sensorsize = 5
+    ">config.ini
+(crontab -l; echo "* * * * * /home/pi/database/sync.sh") | sort -u | crontab -
